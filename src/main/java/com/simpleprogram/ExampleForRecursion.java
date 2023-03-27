640e2c9b2881d988c55480d1a424a7e68e8f8b97
Madhuri Kulle <kullemadhuri2@gmail.com>
2023-03-27 12:25:07 +0530

    Example of Recursion

---
+++ src/main/java/com/simpleprogram/ExampleForRecursion.java
@@ -0,0 +1,14 @@
+package com.simpleprogram;
+
+public class ExampleForRecursion {
+
+	int booking() {
+		return (true ? null : 0);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ExampleForRecursion c= new ExampleForRecursion();
+		c.booking();
+	}
+
+}
